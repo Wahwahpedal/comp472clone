4facdc357f95c8b3bc9f94f018ebbbd0e3a95d55
Tara <taradawnseal@gmail.com>
2019-11-03 21:40:48 -0500

    Fixed bug

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 # This is the main class where the game is played
 from colorama import Fore, Back, Style
 from Game import Game
-from Nodes import TreeNodes
+from Nodes import Nodes
 
 
 # Intro Message
@@ -24,7 +24,7 @@ if (version == "C") or (version == "c"):
     startGame.getBoard().printBoardColors()
     print(Style.RESET_ALL + "=====")
     print("Let's get started!")
-    testing = TreeNodes(startGame.getBoard(), startGame.getPlayer1(), startGame.getPlayer2()) #NOTE: USED FOR TESTING
+    testing = Nodes(startGame.getBoard(), startGame.getPlayer1(), startGame.getPlayer2()) #NOTE: USED FOR TESTING
     testing.generateChildren(1) #NOTE: USED FOR TESTING
     #print(startGame.computerPlaceToken()) For testing
     startGame.playGameWithComputer()
--- Nodes.py
+++ Nodes.py
@@ -4,12 +4,13 @@ from Player import Player
 import numpy
 
 
-class TreeNodes:
+class Nodes:
     def __init__(self, board=Board(), oponentPlayer = Player(), computerPlayer = Player()):
         self.parentBoard = board.cloneBoard()
-        self.childrenBoards = []
+        self.childrenBoards = "null"
         self.opponentPlayer = oponentPlayer
         self.computerPlayer = computerPlayer
+        self.value = 0
 
 
     def generateChildren(self, depth):
@@ -23,13 +24,13 @@ class TreeNodes:
             for j in range(0, 10):
                 if self.parentBoard.getCoordinate(i,j).getOwner().getName() == "null":
                     tempBoard = self.parentBoard
-                    self.childrenBoards.append(tempBoard.updateBoardWithPlayer(i, j, player))
-        print(len(self.childrenBoards)) #NOTE: USED FOR TESTING
+                    self.childrenBoards = Nodes(self.parentBoard.updateBoardWithPlayer(i, j, player), self.opponentPlayer, self.computerPlayer)
+
         #for i in len(self.childrenBoards)): #NOTE: Need to figure this out with another depth
             #self.GenerateChildren(depth - 1);
 
-    def calculateScore(self, childBoard, index, player = Player()):  #Used to calculate the score of the board but store it in the children's trees
-        xCoordinate = self.childrenBoards[index].getCoordinate()
+    def calculateScore(self, childBoard, index, player = Player()):  #Used to calculate the score of the board
+        # xCoordinate = self.childrenBoards[index].getCoordinate()
         totalPoints = 0
         for i in range(0, 12):
             for j in range(0, 10):

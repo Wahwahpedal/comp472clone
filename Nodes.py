af40094b24cb166ae4388ea9ad0cd6174c176900
SwatiPareek <38918266+SwatiPareek@users.noreply.github.com>
2019-11-05 19:17:31 -0500

    Function to add buffer to rectangle

--- Nodes.py
+++ Nodes.py
@@ -1,8 +1,7 @@
 # This class will be used for creating the boards to analyze the depths
 from Board import Board
 from Player import Player
-from Game import Game
-import numpy
+import numpy as np
 
 
 class Nodes:
@@ -16,33 +15,58 @@ class Nodes:
         self.yvalue = y
         self.game = game
 
+    def limitSearchSpace(self, buffer=1):
+        if (self.game.rectangleCoordinates[0] - buffer) < 0:
+            minX = 0
+        else:
+            minX = self.game.rectangleCoordinates[0] - buffer
+
+        if (self.game.rectangleCoordinates[1] + buffer) > 11:
+            maxX = 12
+        else:
+           maxX = self.game.rectangleCoordinates[1] + buffer + 1
+
+        if (self.game.rectangleCoordinates[2] - buffer) < 0:
+            minY = 0
+        else:
+            minY = self.game.rectangleCoordinates[2] - buffer
+
+        if (self.game.rectangleCoordinates[3] + buffer) > 9:
+            maxY = 10
+        else:
+            maxY = self.game.rectangleCoordinates[3] + buffer + 1
+
+        print("Rectangle")
+        print(self.game.rectangleCoordinates)
+
+        arr = [minX, maxX, minY, maxY]
+        return arr
 
     def generateChildren(self, depth):
         if depth <= 0:
             return
-        minX = self.game.rectangleCoordinates[0]
-        maxX = self.game.rectangleCoordinates[1]
-        minY = self.game.rectangleCoordinates[2]
-        maxY = self.game.rectangleCoordinates[3]
-        print(self.game.rectangleCoordinates)
-
+        arr = self.limitSearchSpace(depth)  # MAY WANT TO CHANGE BUFFER
+        print("THIS IS THE RECTANGLE BOUNDARIES")   # Testing
+        print(arr)  # Testing
         if depth == 1:
             player = self.computerPlayer
         elif depth == 2:
             player = self.opponentPlayer
-        for i in range(minX, maxX):
-            for j in range(minY, maxY):
+        for i in range(arr[0],arr[1]):
+            for j in range(arr[2], arr[3]):
                 if self.parentBoard.getCoordinate(i,j).getOwner().getName() == "null":
                     self.childrenBoards = Nodes(self.game, self.parentBoard.updateBoardWithPlayer(i, j, player), self.opponentPlayer, self.computerPlayer, i, j)
+
 
         #for i in len(self.childrenBoards)): #NOTE: Need to figure this out with another depth
             #self.GenerateChildren(depth - 1);
 
-    def calculateScore(self, childBoard, index, player = Player()):  #Used to calculate the score of the board
+    def calculateScore(self, childBoard, index, depth, player = Player()):  #Used to calculate the score of the board
+        arr = self.limitSearchSpace(depth)  # MAY WANT TO CHANGE BUFFER
         # xCoordinate = self.childrenBoards[index].getCoordinate()
         totalPoints = 0
-        for i in range(0, 12):
-            for j in range(0, 10):
+        for i in range(arr[0], arr[1]):
+            for j in range(arr[2], arr[3]):
                 if childBoard[index].getCoordinate(i,j).getOwner().getName() == player.getName():
                     totalPoints = totalPoints + 5 #This is used just for testing at the moment
         return totalPoints
